007bd4fc6370f62d7e6cb4a7971e756b1d0cfbdd
Linas Medžiūnas <linas.medziunas@gmail.com>
2020-02-28 23:03:51 +0200

    Unrolling, step 2

--- src/test/scala/search/engine/IndexOfRepeatedTest.scala
+++ src/test/scala/search/engine/IndexOfRepeatedTest.scala
@@ -1,11 +1,15 @@
 package search.engine
 
+import java.nio.charset.Charset
+
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import search.algorithm.{AhoCorasic, KnuthMorrisPratt, SearchAlgorithm, ShiftingBitMask}
 
 abstract class IndexOfRepeatedTest(algorithm: Array[Byte] => SearchAlgorithm)
   extends AnyWordSpec with Matchers with ByteBufferConverter with IndexOfSupport {
+
+  private val utf8 = Charset.forName("Utf-8")
 
   "find the needle repeatedly" in {
 
@@ -29,6 +33,28 @@ abstract class IndexOfRepeatedTest(algorithm: Array[Byte] => SearchAlgorithm)
     indexOf(haystack, processor) shouldEqual 0
     indexOf(haystack, processor) shouldEqual 3
     indexOf(haystack, processor) shouldEqual 6
+    indexOf(haystack, processor) shouldEqual -1
+  }
+
+  "find a long needle" in {
+
+    val haystack = toByteBuffer(Array.tabulate(256)(_.toByte))
+    val needle = Array.tabulate(57)(x => (x + 16).toByte)
+
+    val processor = algorithm(needle).newProcessor
+
+    indexOf(haystack, processor) shouldEqual 16
+    indexOf(haystack, processor) shouldEqual -1
+  }
+
+  "find UTF symbols" in {
+
+    val haystack = toByteBuffer("Šešios žąsys su šešiais žąsyčiais.".getBytes(utf8))
+
+    val processor = algorithm("žąs".getBytes(utf8)).newProcessor
+
+    indexOf(haystack, processor) shouldEqual 9
+    indexOf(haystack, processor) shouldEqual 30
     indexOf(haystack, processor) shouldEqual -1
   }
 
